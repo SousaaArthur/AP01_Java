7fdb2e046f935711b000edc934b8344f0e7e9b0c
SousaaArthur <arthursousaa2004@gmail.com>
2024-09-21 01:59:15 -0300

    Atualização exercicio ADO AP01

---
+++ src/Aula01/Exercicio/Exercicio10.java
@@ -0,0 +1,30 @@
+package Aula01.Exercicio;
+
+import java.util.Scanner;
+
+// Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas 
+// efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de 
+// comissão sobre suas vendas efetuadas, calcular e imprimir o total a receber no final do mês.
+public class Exercicio10 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Insira o nome do vendedor: ");
+    String name = sc.next();
+    System.out.print("Insira o salário do vendedor: ");
+    double wage = sc.nextDouble();
+    System.out.print("Insira a vendas do vendedor: ");
+    double sales = sc.nextDouble();
+
+    double commission = sales * 0.15;
+    double fullSalary = wage + commission; 
+
+    System.out.println("Nome vendedor: " + name);
+    System.out.printf("Salário vendedor: %.2f%n", wage);
+    System.out.printf("Total de vendas: %.2f%n", sales);
+    System.out.printf("Comissão: %.2f%n", commission);
+    System.out.printf("Salário no final do mês: %.2f%n", fullSalary);
+
+    sc.close();
+  }
+}
